85572fec2ce629df591bc68ca516f1cfc9b8326b
luceroGS <72269843+luceroGS@users.noreply.github.com>
2021-06-14 11:38:56 -0500

    Fragments-Nuevos cambios

--- app/src/main/java/com/example/shoesworld/MainActivity.java
+++ app/src/main/java/com/example/shoesworld/MainActivity.java
@@ -39,7 +39,7 @@ public class MainActivity extends AppCompatActivity {
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         mAppBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow)
+                R.id.nav_home, R.id.nav_agregar, R.id.nav_buscar, R.id.nav_creditos, R.id.nav_inactivos, R.id.nav_productos)
                 .setDrawerLayout(drawer)
                 .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
