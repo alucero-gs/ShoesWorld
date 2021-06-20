c8cdbb1496f2166a7d349d5c35252066bc760838
luceroGS <72269843+luceroGS@users.noreply.github.com>
2021-06-19 23:51:10 -0500

    GPS

--- app/src/main/java/com/example/shoesworld/ui/creditos/CreditosFragment.java
+++ app/src/main/java/com/example/shoesworld/ui/creditos/CreditosFragment.java
@@ -2,6 +2,7 @@ package com.example.shoesworld.ui.creditos;
 
 import androidx.lifecycle.ViewModelProvider;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
@@ -11,10 +12,13 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
 
 import com.example.shoesworld.R;
 
-public class CreditosFragment extends Fragment {
+public class CreditosFragment extends Fragment implements View.OnClickListener{
+    Button btnUbicacion;
 
     private CreditosViewModel mViewModel;
 
@@ -25,8 +29,23 @@ public class CreditosFragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_creditos, container, false);
+        View root = inflater.inflate(R.layout.fragment_creditos, container, false);
+
+        mapa(root);
+        return root;
+
     }
+
+    public void mapa(View root){
+        btnUbicacion = root.findViewById(R.id.btnUbicacion);
+        btnUbicacion.setOnClickListener((View.OnClickListener) this);
+
+
+    }
+
+
+
+
 
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
@@ -35,4 +54,14 @@ public class CreditosFragment extends Fragment {
         // TODO: Use the ViewModel
     }
 
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.btnUbicacion:
+                Intent nuevoI = new Intent(getContext(), MapsActivity.class);
+                startActivity(nuevoI);
+                onStop();
+                break;
+        }
+    }
 }
---
+++ app/src/main/java/com/example/shoesworld/ui/creditos/MapsActivity.java
@@ -0,0 +1,82 @@
+package com.example.shoesworld.ui.creditos;
+
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import androidx.fragment.app.FragmentActivity;
+
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+
+import com.example.shoesworld.R;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.CameraPosition;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+
+    private GoogleMap mMap;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_maps2);
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+        getLocalizacion();
+    }
+
+    private void getLocalizacion() {
+        int permiso = ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_BACKGROUND_LOCATION);
+        if (permiso == PackageManager.PERMISSION_DENIED) {
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.ACCESS_FINE_LOCATION)) {
+
+            } else {
+                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 1);
+            }
+        }
+    }
+
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+
+        // Add a marker in Sydney and move the camera
+        LatLng ubicacion = new LatLng(19.19885187178827, -99.46307600735358);
+        mMap.addMarker(new MarkerOptions().position(ubicacion).title("Ubicacion Shoes World"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(ubicacion));
+        CameraPosition cameraPosition = new CameraPosition.Builder()
+                .target(ubicacion)
+                .zoom(17)
+                .bearing(90)
+                .tilt(45)
+                .build();
+        mMap.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
+    }
+}
