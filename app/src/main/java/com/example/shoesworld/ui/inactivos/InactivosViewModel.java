91606354bb171cf7550774f91531c9d6571cb755
ShasselZamora <shassel_zamora@yahoo.es>
2021-06-20 20:39:45 -0500

    Productos agotados igual que en stock (lista)

--- app/src/main/java/com/example/shoesworld/ui/inactivos/InactivosViewModel.java
+++ app/src/main/java/com/example/shoesworld/ui/inactivos/InactivosViewModel.java
@@ -1,7 +1,18 @@
 package com.example.shoesworld.ui.inactivos;
 
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
 public class InactivosViewModel extends ViewModel {
-    // TODO: Implement the ViewModel
+    private MutableLiveData<String> mText;
+
+    public InactivosViewModel() {
+        mText = new MutableLiveData<>();
+        mText.setValue("Hola soy stock");
+    }
+
+    public LiveData<String> getText() {
+        return mText;
+    }
 }
